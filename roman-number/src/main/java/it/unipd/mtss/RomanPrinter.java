5765f2dad40c764d6513df2dab4bbe9f4ef95d19
Alba <149021742+Albahui@users.noreply.github.com>
2025-05-09 14:10:06 +0200

    Update RomanPrinter.java

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -1,10 +1,85 @@
+//Maria Fuensanta Trigueros Hernandez [2133487]
+//Alba Hui Larrosa Serrano [2133266]
+
+
 package it.unipd.mtss;
 public class RomanPrinter {
-  public static String print(int num){
-  return printAsciiArt(IntegerToRoman.convert(num));
-  }
+  private static final String[] I_Rep = {
+    " _____ ",
+    "|_   _|",
+    "  | |  ",
+    "  | |  ",
+    "  | |  ",
+    " _| |_ ",
+    "|_____|"
+  };
+
+  private static final String[] V_Rep = {
+    "___        ___",
+    "\\ \\     / /",
+    " \\ \\   / / ",
+    "  \\ \\ / /",
+    "   \\    /",
+    "    \\  /",
+    "     \\/"
+  };
+
+  private static final String[] X_Rep = {
+    " ___    ___ ",
+    " \  \  /  /",
+    "  \  \/  /",
+    "   \    /",
+    "   /    \",
+    "  /  /\  \",
+    " /__/  \__\"
+  };
+
+  private static final String[] L_Rep = {
+    " __",
+    "|  |",
+    "|  |",
+    "|  |",
+    "|  |",
+    "|  |_____",
+    "|________|",
+  };
+
+  private static final String[] C_Rep = {
+    " ________",
+    "/  ______|",
+    "|  |",
+    "|  |",
+    "|  |",
+    "|  |______",
+    "\_________|"
+  };
+
+  private static final String[] D_Rep = {
+    " _______",
+    "|  ____  \",
+    "|  |  |  |",
+    "|  |  |  |",
+    "|  |  |  |",
+    "|  |__|  |",
+    "|_______ /"
+  };
+
+  private static final String[] M_Rep = {
+    " ___    ___",
+    "|   \  /   |",
+    "|    \/    |",
+    "|  |\  /|  |",
+    "|  | \/ |  |",
+    "|  |    |  |",
+    "|__|    |__|"
+  };
+
   private static String printAsciiArt(String romanNumber){
-  //TODO
-  return null;
+
+  }
+  
+  
+  public static String print(int num){
+    return printAsciiArt(IntegerToRoman.convert(num));
   }
 }
