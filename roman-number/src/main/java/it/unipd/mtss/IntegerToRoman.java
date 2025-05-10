031758da1f73afa0eb0667411fa7f34f1de8b00f
Alba <149021742+Albahui@users.noreply.github.com>
2025-05-10 14:23:43 +0200

    Update IntegerToRoman.java

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,3 +1,8 @@
+////////////////////////////////////////////////////////////////////
+// [Maria Fuensanta] [Trigueros Hernandez] [2133487]
+// [Alba Hui] [Larrosa Serrano] [2133266]
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 public class IntegerToRoman {
   public static String convert(int number){
