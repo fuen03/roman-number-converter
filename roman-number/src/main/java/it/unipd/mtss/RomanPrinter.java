dcb558237753a9a6a791651d1d54d6dedfc11452
Alba <149021742+Albahui@users.noreply.github.com>
2025-05-10 13:15:46 +0200

    Update RomanPrinter.java

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -2,136 +2,130 @@
 //Alba Hui Larrosa Serrano [2133266]
 
 package it.unipd.mtss;
+
 public class RomanPrinter {
-  private static final String[] I_Rep = {
-    " _____ ",
-    "|_   _|",
-    "  | |  ",
-    "  | |  ",
-    "  | |  ",
-    " _| |_ ",
-    "|_____|"
-  };
+    private static final String[] I_Rep = {
+        " _____ ",
+        "|_   _|",
+        "  | |  ",
+        "  | |  ",
+        "  | |  ",
+        " _| |_ ",
+        "|_____|"
+    };
 
-  private static final String[] V_Rep = {
-    "___        ___",
-    "\\ \\     / /",
-    " \\ \\   / / ",
-    "  \\ \\ / /",
-    "   \\    /",
-    "    \\  /",
-    "     \\/"
-  };
+    private static final String[] V_Rep = {
+        "___        ___",
+        "\\ \\      / /",
+        " \\ \\    / / ",
+        "  \\ \\  / /  ",
+        "   \\ \\/ /   ",
+        "    \\   /    ",
+        "     \\ /     "
+    };
 
-  private static final String[] X_Rep = {
-    " ___    ___ ",
-    " \  \  /  / ",
-    "  \  \/  /  ",
-    "   \    /   ",
-    "   /    \   ",
-    "  /  /\  \  ",
-    " /__/  \__\ "
-  };
+    private static final String[] X_Rep = {
+        " ___    ___ ",
+        " \\ \\  / / ",
+        "  \\ \\/ /  ",
+        "   \\   /   ",
+        "   /   \\   ",
+        "  / /\\ \\  ",
+        " /_/  \\_\\ "
+    };
 
-  private static final String[] L_Rep = {
-    " __    ",
-    "|  |   ",
-    "|  |   ",
-    "|  |   ",
-    "|  |   ",
-    "|  |_____",
-    "|________|",
-  };
+    private static final String[] L_Rep = {
+        " __      ",
+        "|  |     ",
+        "|  |     ",
+        "|  |     ",
+        "|  |     ",
+        "|  |_____ ",
+        "|________|"
+    };
 
-  private static final String[] C_Rep = {
-    " ________  ",
-    "/  ______| ",
-    "|  |       ",
-    "|  |       ",
-    "|  |       ",
-    "|  |______ ",
-    "\_________|"
-  };
+    private static final String[] C_Rep = {
+        " ________  ",
+        "/  ______| ",
+        "|  |       ",
+        "|  |       ",
+        "|  |       ",
+        "|  |______ ",
+        "\\________|"
+    };
 
-  private static final String[] D_Rep = {
-    " _______   ",
-    "|  ____  \ ",
-    "|  |  |  | ",
-    "|  |  |  | ",
-    "|  |  |  | ",
-    "|  |__|  | ",
-    "|_______ / "
-  };
+    private static final String[] D_Rep = {
+        " _______   ",
+        "|  ____  \\ ",
+        "|  |  |  | ",
+        "|  |  |  | ",
+        "|  |  |  | ",
+        "|  |__|  | ",
+        "|_______/  "
+    };
 
-  private static final String[] M_Rep = {
-    " ___    ___  ",
-    "|   \  /   | ",
-    "|    \/    | ",
-    "|  |\  /|  | ",
-    "|  | \/ |  | ",
-    "|  |    |  | ",
-    "|__|    |__| "
-  };
+    private static final String[] M_Rep = {
+        " ___    ___  ",
+        "|   \\  /   | ",
+        "|    \\/    | ",
+        "|  |\\  /|  | ",
+        "|  | \\/ |  | ",
+        "|  |     |  | ",
+        "|__|     |__| "
+    };
 
-  private static String printAsciiArt(String romanNumber){
-    if (romanNumber == null || romanNumber.isEmpty()){
-      throw new IllegalArgumentException("The roman number could not be null or empty");
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
     }
 
-    if (!romanNumber.matches("[IVXLCDM]+$")){
-        throw new IllegalArgumentException("The roman number has invalid characters");
+    private static String printAsciiArt(String romanNumber) {
+        if (romanNumber == null || romanNumber.isEmpty()) {
+            throw new IllegalArgumentException("The roman number could not be null or empty");
+        }
+
+        if (!romanNumber.matches("[IVXLCDM]+")) {
+            throw new IllegalArgumentException("The roman number has invalid characters");
+        }
+
+        String[] result = new String[7];
+        for (int i = 0; i < 7; i++) {
+            result[i] = "";
+        }
+
+        for (int i = 0; i < romanNumber.length(); i++) {
+            char romanChar = romanNumber.charAt(i);
+            String[] rep;
+
+            switch (romanChar) {
+                case 'I':
+                    rep = I_Rep;
+                    break;
+                case 'V':
+                    rep = V_Rep;
+                    break;
+                case 'X':
+                    rep = X_Rep;
+                    break;
+                case 'L':
+                    rep = L_Rep;
+                    break;
+                case 'C':
+                    rep = C_Rep;
+                    break;
+                case 'D':
+                    rep = D_Rep;
+                    break;
+                case 'M':
+                    rep = M_Rep;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid character: " + romanChar);
+            }
+
+            for (int j = 0; j < 7; j++) {
+                result[j] += rep[j];
+            }
+        }
+        return String.join("\n", result);
     }
-  }
-
-  String[] result = new String[6];
-  for (int i = 0; i < 6; i++){
-    result[i]="";
-  }
-
-  for (int i = 0; 9 < romanNumber.length(); i++){
-    char romanChar = romanNumber.charAt(i);
-    String[] rep;
-
-    switch(romanChar){
-        case "I":
-            rep = I_Rep;
-            break;
-
-        case "V":
-            rep = V_Rep;
-            break;
-        
-        case "X":
-            rep = X_Rep;
-            break;
-            
-        case "L":
-            rep = L_Rep;
-            break;
-
-        case "C":
-            rep = C_Rep;
-            break;
-            
-        case "D":
-            rep = D_Rep;
-            break;
-        
-        case "M":
-            rep = M_Rep;
-            break;
-        
-        default:
-            throw new IllegalArgumentException("Invalid character: " + romanChar);
-    }
-
-    for (int j = 0; j < 6; j++){
-        result[j] += rep[j];
-    }
-  }
-  return String.join("\n", result);
-  
-  public static String print(int num){
-    return printAsciiArt(IntegerToRoman.convert(num));
-  }
 }
