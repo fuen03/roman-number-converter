61982ab395b14ac0b2246786b93e4c35a5f0c6df
Alba <149021742+Albahui@users.noreply.github.com>
2025-05-10 14:19:59 +0200

    Create IntegerToRomanTest.java

---
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,42 @@
+//
+//
+
+package it.unipd.mtss;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IntegerToRomanTest {
+
+    @Test
+    public void testConvertForOne() {
+        int num = 1;
+        String exp = "I";
+
+        String res = IntegerToRoman.convert(num);
+
+        assertEquals("The number 1 has to be I", exp, res);
+    }
+
+    @Test
+    public void testConvertForTwo() {
+        int num = 2;
+        String exp = "II";
+
+        String res = IntegerToRoman.convert(num);
+
+        assertEquals("The number 2 has to be II", exp, res);
+    }
+
+    @Test
+    public void testConvertForThree() {
+        int num = 3;
+        String exp = "III";
+
+        String res = IntegerToRoman.convert(num);
+
+        assertEquals("The number 3 has to be III", exp, res);
+    }
+
+
+}
