c16b9fd307446ca9569a13265032af4fce356695
Alba <149021742+Albahui@users.noreply.github.com>
2025-05-11 14:13:56 +0200

    Update IntegerToRomanTest.java

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -13,9 +13,7 @@ public class IntegerToRomanTest {
     public void testConvertForOne() {
         int num = 1;
         String exp = "I";
-
         String res = IntegerToRoman.convert(num);
-
         assertEquals("The number 1 has to be I", exp, res);
     }
 
@@ -23,9 +21,7 @@ public class IntegerToRomanTest {
     public void testConvertForTwo() {
         int num = 2;
         String exp = "II";
-
         String res = IntegerToRoman.convert(num);
-
         assertEquals("The number 2 has to be II", exp, res);
     }
 
@@ -33,11 +29,48 @@ public class IntegerToRomanTest {
     public void testConvertForThree() {
         int num = 3;
         String exp = "III";
-
         String res = IntegerToRoman.convert(num);
-
         assertEquals("The number 3 has to be III", exp, res);
     }
 
+    @Test
+    public void testConvertForFour() {
+        int num = 4;
+        String exp = "IV";
+        String res = IntegerToRoman.convert(num);
+        assertEquals("The number 4 has to be IV", exp, res);
+    }
+
+    @Test
+    public void testConvertForFive() {
+        int num = 5;
+        String exp = "V";
+        String res = IntegerToRoman.convert(num);
+        assertEquals("The number 5 has to be V", exp, res);
+    }
+
+    @Test
+    public void testConvertForSix() {
+        int num = 6;
+        String exp = "VI";
+        String res = IntegerToRoman.convert(num);
+        assertEquals("The number 6 has to be VI", exp, res);
+    }
+
+    @Test
+    public void testConvertForNine() {
+        int num = 9;
+        String exp = "IX";
+        String res = IntegerToRoman.convert(num);
+        assertEquals("The number 9 has to be IX", exp, res);
+    }
+
+    @Test
+    public void testConvertForTen() {
+        int num = 10;
+        String exp = "X";
+        String res = IntegerToRoman.convert(num);
+        assertEquals("The number 10 has to be X", exp, res);
+    }
 
 }
